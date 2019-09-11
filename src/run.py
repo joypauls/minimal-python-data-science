54fe877c40ad53a5f625c54f1ceba30be210b797
joypauls <joeypaulsen3@gmail.com>
2019-09-11 03:52:30 +0000

    init

---
+++ src/run.py
@@ -0,0 +1,11 @@
+import logging
+
+logger = logging.getLogger(__name__)
+logger.basicConfig(level=logging.DEBUG)
+
+logging.info('Script Starting')
+
+# stuff
+print("doing stuff")
+
+logging.info('Script Finsihed')
